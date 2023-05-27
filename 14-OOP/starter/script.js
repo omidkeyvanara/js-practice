46134cda2adbeb9e9fb34f71ce424e9a60972d77
omid <omid.amin90@gmail.congit config --global user.email omid.amin90@gmail.com>
2023-05-27 15:36:26 +0330

    function constructor

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person(`jonas`, 1991);
+console.log(jonas);
+
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+console.log(Person.prototype);
+
+jonas.calcAge();
