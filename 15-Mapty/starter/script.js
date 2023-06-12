4bec4931cc69f2d751dd4f962b54859213da8d49
omid <omid.amin90@gmail.congit config --global user.email omid.amin90@gmail.com>
2023-06-12 13:08:55 +0330

    start of the mapty app

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,12 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (location) {
+    console.log(location);
+  },
+  function () {
+    prompt(`we can't acces your location`);
+  }
+);
