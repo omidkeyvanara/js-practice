adbfaf6556bc43078c47ef70028b3ca3100ae748
omid <omid.amin90@gmail.congit config --global user.email omid.amin90@gmail.com>
2023-06-13 13:29:55 +0330

    leaflet library

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -12,10 +12,24 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation.getCurrentPosition(
-  function (location) {
-    console.log(location);
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
-    prompt(`we can't acces your location`);
+    alert(`we can't acces your location`);
   }
 );
