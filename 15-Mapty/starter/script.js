c5b159556f583239858aa81ef9dc51dfbd98491f
omid <omid.amin90@gmail.congit config --global user.email omid.amin90@gmail.com>
2023-06-19 12:01:26 +0330

    refactering App class

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,76 +11,85 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener(`submit`, this._newWorkout.bind(this));
+    // 1*
+    inputType.addEventListener(`change`, this._toggleElevationField);
+  }
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert(`we can't acces your location`);
+        }
+      );
+  }
+
+  _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
-    map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
     // HANDLING CLICKS ON MAP
-    //  In the Leaflet library, map.on() is a method that allows you to attach an event listener to a specific map instance.نمیتوان به یک نقشه ایونت لیسنر اضافه کرد، از
-
-    class App{
-
-      _getPosition(){}
-
-      _loadMap(){}
-
-      _showForm(){}
-
-      _toggleElevationField(){}
-    }
-
-    map.on(`click`, function (mapE) {
-      console.log(mapE);
-      mapEvent = mapE;
-      form.classList.remove(`hidden`);
-      inputDistance.focus();
-    });
-  },
-  function () {
-    alert(`we can't acces your location`);
+    this.#map.on(`click`, this._showForm.bind(this));
+    // 2*
   }
-);
 
-form.addEventListener(`submit`, function (e) {
-  e.preventDefault();
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove(`hidden`);
+    inputDistance.focus();
+  }
 
-  // CLEAR INPUT FIELDS
-  inputCadence.value =
-    inputDistance.value =
-    inputDuration.value =
-    inputElevation.value =
-      ``;
+  _toggleElevationField() {
+    inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+    inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+  }
 
-  // DISPLAY MARKER
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxwidth: 250,
-        minwidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: `running-popup`,
-      })
-    )
-    .setPopupContent(`workout`)
-    .openPopup();
-});
+  _newWorkout(e) {
+    e.preventDefault();
 
-inputType.addEventListener(`change`, function () {
-  inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
-  inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`);
-});
+    // CLEAR INPUT FIELDS
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        ``;
+
+    // DISPLAY MARKER
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxwidth: 250,
+          minwidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `running-popup`,
+        })
+      )
+      .setPopupContent(`workout`)
+      .openPopup();
+  }
+}
+const app = new App();
+
+// *2
+//  In the Leaflet library, map.on() is a method that allows you to attach an event listener to a specific map instance.نمیتوان به یک نقشه ایونت لیسنر اضافه کرد، از
+
+// 1* در ایونت لیسنر کلمه دیس به المانی که رخداد به آن اضافه شده الصاق میشود، یعنی به دام موردنظر ما، ولی در اینجا ما نیاز داریم که دیس به آبجکت اصلی ما ارجاع دهد پس به کمک متد بایند ارجاع را اصلاح میکنیم.
