ca262ef86e363701e171ecd0f700361b505408c3
omid <omid.amin90@gmail.congit config --global user.email omid.amin90@gmail.com>
2023-06-26 13:12:19 +0330

    working on workout class

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -22,6 +22,7 @@ class Workout {
 }
 
 class Running extends Workout {
+  type = `running`;
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
@@ -33,8 +34,9 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type = `cycling`;
   constructor(coords, distance, duration, elevationGain) {
-    super(coords, distance, duration, elevationGain);
+    super(coords, distance, duration);
     this.elevationGain = elevationGain;
   }
 
@@ -48,6 +50,7 @@ class Cycling extends Workout {
 class App {
   #map;
   #mapEvent;
+  #workout = [];
   constructor() {
     this._getPosition();
     form.addEventListener(`submit`, this._newWorkout.bind(this)); //*1*
@@ -93,9 +96,15 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
     e.preventDefault();
 
     // get data from form
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
@@ -106,36 +115,32 @@ class App {
     if (type === `running`) {
       const cadence = +inputCadence.value;
       if (
-        !Number.isFinite(distance) ||
-        !Number.isFinite(duration) ||
-        !Number.isFinite(cadence)
+        !validInputs(cadence, distance, duration) ||
+        !allPositive(cadence, distance, duration)
       )
         return alert(`input have to be positive number`);
+        workout = new Running([lat, lng], cadence, distance, duration);
     }
+
+
     // if workout cycling, create cycling object
     if (type === `cycling`) {
       const elevation = +inputElevation.value;
-      if (!Number.isFinite(elevation))
+      if (
+        !validInputs(elevation, distance, duration) ||
+        !allPositive(distance, duration)
+      )
         return alert(`input have to be positive number`);
+
+      workout = new Cycling([lat, lng], distance, duration,elevation);
     }
 
     // add new object to the workout array
+    this.#workout.push(workout);
 
     // render workout on map as marker
-    const { lat, lng } = this.#mapEvent.latlng;
-    L.marker([lat, lng])
-      .addTo(this.#map)
-      .bindPopup(
-        L.popup({
-          maxwidth: 250,
-          minwidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: `running-popup`,
-        })
-      )
-      .setPopupContent(`workout`)
-      .openPopup();
+    this.renderWorkoutMarker(workout);
+
     // render workout on list
 
     // hide form + clear input fields
@@ -145,10 +150,25 @@ class App {
       inputElevation.value =
         ``;
   }
+  renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxwidth: 250,
+          minwidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      )
+      .setPopupContent(`workout`)
+      .openPopup();
+  }
 }
 const app = new App();
 
+// 1* در ایونت لیسنر کلمه دیس به المانی که رخداد به آن اضافه شده الصاق میشود، یعنی به دام موردنظر ما، ولی در اینجا ما نیاز داریم که دیس به آبجکت اصلی ما ارجاع دهد پس به کمک متد بایند ارجاع را اصلاح میکنیم.
+
 // *2
 //  In the Leaflet library, map.on() is a method that allows you to attach an event listener to a specific map instance.نمیتوان به یک نقشه ایونت لیسنر اضافه کرد، از
-
-// 1* در ایونت لیسنر کلمه دیس به المانی که رخداد به آن اضافه شده الصاق میشود، یعنی به دام موردنظر ما، ولی در اینجا ما نیاز داریم که دیس به آبجکت اصلی ما ارجاع دهد پس به کمک متد بایند ارجاع را اصلاح میکنیم.
