50b0c400845a6caa1ba356fefe7f2afa2061b105
omid <omid.amin90@gmail.congit config --global user.email omid.amin90@gmail.com>
2023-06-28 13:21:50 +0330

    renderWorkoutMarker added

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -150,7 +150,7 @@ class App {
       inputElevation.value =
         ``;
   }
-  renderWorkoutMarker(workout) {
+  _renderWorkoutMarker(workout) {
     L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
@@ -165,6 +165,21 @@ class App {
       .setPopupContent(`workout`)
       .openPopup();
   }
+
+  _renderWorkoutMarker(workout){
+    const html = `<li class="workout workout--${workout.name}" data-id="${workout.id}">
+    <h2 class="workout__title">Running on April 14</h2>
+    <div class="workout__details">
+      <span class="workout__icon">${workout.name=== `running`?`🏃‍♂️`:`🚴‍♀️`}</span>
+      <span class="workout__value">${workout.distance}</span>
+      <span class="workout__unit">km</span>
+    </div>
+    <div class="workout__details">
+      <span class="workout__icon">⏱</span>
+      <span class="workout__value">${workout.duration}</span>
+      <span class="workout__unit">min</span>
+    </div>`
+  } 
 }
 const app = new App();
 
